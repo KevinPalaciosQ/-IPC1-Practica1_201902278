ae08b7ee252afcdef431d5cd501ab403cd4c6665
KevinPalaciosQ <3001877180101@ingenieria.usac.edu.gt>
2022-02-01 23:09:15 -0600

    ya hice el menú ;)

--- Practica1/src/practica1/Practica1.java
+++ Practica1/src/practica1/Practica1.java
@@ -1,5 +1,46 @@
 package practica1;
+import java.util.Scanner;
 public class Practica1 {
     public static void main(String[] args) {
-    }  
-}
+            Scanner leer = new Scanner(System.in);
+            int opcion=0;
+            do{
+            try{
+             //Menu Juego
+        System.out.println("PACMAN - IPC 1 - 2022");
+        System.out.println("----------------------");
+        System.out.println("1.       INICIAR JUEGO");
+        System.out.println("2     TABLA POSICIONES");
+        System.out.println("3.               SALIR");
+        System.out.println("----------------------");
+        System.out.println("INGRESE UNA OPCION...");
+        opcion = leer.nextInt();
+        switch(opcion){
+            case 1: //Iniciar Juego
+                System.out.println("*****************************");
+                System.out.println("       INICIAR JUEGO         ");
+                System.out.println("*****************************");
+                break;
+            case 2: // Tabla de Posiciones
+                System.out.println("******************************");
+                System.out.println("*     TABLA DE POSICIONES    *");
+                System.out.println("*****************************");
+                break;
+            case 3: //Salir de Menú
+                System.out.println("*****************************");
+                System.out.println("Adios, tenga un lindo día");
+                System.out.println("*****************************");
+                break;
+            default:
+                System.out.println("No ingrese valores no validos \n");
+        }
+        }
+        catch(Exception e){
+            System.out.println("Solo ingrese valores númericos \n");
+            leer.nextLine();
+        }
+     }while(opcion !=3);      
+    }
+            }
+
+                
